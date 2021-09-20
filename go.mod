834aa45c85cadc15d098f4f8a97019140ac3deb7
xjl <18253040259@163.com>
2021-09-20 13:32:23 +0000

    分布式缓存

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module cache
 
 go 1.16
 
-require github.com/go-redis/redis v6.15.9+incompatible
+require (
+	github.com/go-redis/redis v6.15.9+incompatible
+	github.com/hashicorp/memberlist v0.2.4
+	stathat.com/c/consistent v1.0.0
+)
--- server/http/server.go
+++ server/http/server.go
@@ -2,19 +2,22 @@ package http
 
 import (
 	"cache/server/cache"
+	"cache/server/cluster"
 	"net/http"
 )
 
 type Server struct {
 	cache.Cache
+	cluster.Node
 }
 
 func (s *Server) Listen() {
 	http.Handle("/cache/", s.cacheHandler())
 	http.Handle("/status", s.statusHandler())
-	http.ListenAndServe(":12345", nil)
+	http.Handle("/cluster", s.clusterHandler())
+	http.ListenAndServe(s.Addr()+":12345", nil)
 }
 
-func New(c cache.Cache) *Server {
-	return &Server{c}
+func New(c cache.Cache, n cluster.Node) *Server {
+	return &Server{c, n}
 }

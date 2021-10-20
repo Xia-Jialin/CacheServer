612265f4ec1b4ea6e45c30717d27d588a75234f1
xjl <18253040259@163.com>
2021-10-20 15:41:40 +0800

    修改了-node参数的默认值

--- server/main.go
+++ server/main.go
@@ -12,7 +12,7 @@ import (
 func main() {
 	typ := flag.String("type", "inmemory", "cache type")
 	ttl := flag.Int("ttl", 30, "cache time to live")
-	node := flag.String("node", "127.0.0.1", "node address")
+	node := flag.String("node", "0.0.0.0", "node address")
 	clus := flag.String("cluster", "", "cluster address")
 	flag.Parse()
 	log.Println("type is", *typ)
